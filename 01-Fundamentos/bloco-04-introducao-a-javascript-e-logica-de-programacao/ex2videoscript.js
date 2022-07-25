ab8a5ac8215b6affa1d04382bd6339cf3de80534
guisaoliv <guisaoliv@gmail.com>
2022-07-25 20:48:31 -0300

    Organiza diretórios dos dias

---
+++ 01-Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/ex2videoscript.js
@@ -0,0 +1,11 @@
+🚀 //1. Crie uma constante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+const base = 5
+let height = 8
+
+🚀 //2 .Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+const area = base * height;
+console.log(area);
+
+🚀 //3. Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+const perimeter = 5 + 5 + 8 + 8;
+console.log(perimeter);
