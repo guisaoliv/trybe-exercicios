e05d75ad14bec25a15dba7a7339662749ef521d8
guisaoliv <guisaoliv@gmail.com>
2022-07-26 10:16:30 -0300

    Organiza diretórios dos dias

---
+++ 01-Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/ex3videoscript.js
@@ -0,0 +1,29 @@
+// 🚀 1. Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100; 
+const nota = 59;
+
+// 🚀 2. Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações: 
+// • Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
+// • Se a nota for menor que 80 e maior ou igual a 60, imprima "Você está na nossa lista de espera"
+//• Se a nota for menor que 60, imprima "Você foi reprovada(o)"
+
+// 🚀 3.Crie uma estrutura condicional utilizando o if, else if e else para criar o seu algoritmo, e os operadores lógicos que se aplicam a cada situação. 
+
+if(nota >= 80) {
+    console.log("Parabéns, você foi aprovado(a)!");
+}
+else if(nota < 80 && nota >= 60) {
+    console.log("Você está na lista de espera");
+}
+else{
+    console.log("Você foi reprovado(a)")
+}
+
+// 🚀 4.Altere o valor da nota para verificar se as condições que você implementou funcionam;
+
+
+
+
+
+
+
+
