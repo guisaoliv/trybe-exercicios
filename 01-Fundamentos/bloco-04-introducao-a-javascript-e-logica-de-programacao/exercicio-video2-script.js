bbcf699980c7c45a83915f3525b1c5a5934ff039
guisaoliv <guisaoliv@gmail.com>
2022-07-30 18:11:00 -0300

    Organiza diretórios dos dias

--- 01-Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicio-video2-script.js
+++ 01-Fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/exercicio-video2-script.js
@@ -5,3 +5,11 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for (let index = 0; index < groceryList.length; index+=1) {
     console.log(groceryList[index]);
 }
+
+// Exercício For/of - Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for(let lista of names) {
+    console.log(lista);
+}
